f13d4da6ea321f1f1fdc7600a21d11bde4564794
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-02-13 13:24:48 +0100

    Showing user coords using the composable

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
-  <h1>Hello</h1>
+  <div class="header">
+    <h1>Your Position</h1>
+    <p>
+      Latitude: {{ currentPos.lat.toFixed(2) }}, Longitude:
+      {{ currentPos.lng.toFixed(2) }}
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
+import { useGeolocation } from './composables/useGeolocation'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+
+  setup() {
+    // Holds user coords
+    const { coords } = useGeolocation()
+    // Computed to it changes when coords change
+    const currentPos = computed(() => ({
+      lat: coords.value.latitude,
+      lng: coords.value.longitude
+    }))
+
+    return { currentPos }
+  }
 })
 </script>
 
