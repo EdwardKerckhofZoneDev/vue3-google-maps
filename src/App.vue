d446550a80af326370cbabadf2cf723cc6781218
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-02-13 14:28:38 +0100

    Draw line to clicked point

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="header">
-    <h1>Your Position</h1>
-    <p>
-      Latitude: {{ currentPos.lat.toFixed(2) }}, Longitude:
-      {{ currentPos.lng.toFixed(2) }}
-    </p>
+    <div class="user-pos">
+      <h1>Your Position</h1>
+      <p>
+        Latitude: {{ currentPos.lat.toFixed(2) }}, Longitude:
+        {{ currentPos.lng.toFixed(2) }}
+      </p>
+    </div>
+    <div class="clicked-pos">
+      <h1>Clicked Position</h1>
+      <p v-if="clickedPos">
+        Latitude: {{ clickedPos.lat.toFixed(2) }}, Longitude:
+        {{ clickedPos.lng.toFixed(2) }}
+      </p>
+      <p v-else>Click the map to select a position</p>
+    </div>
   </div>
+  <div ref="mapRef" class="google-map"></div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import {
+  computed,
+  defineComponent,
+  onMounted,
+  onUnmounted,
+  ref,
+  watch
+} from 'vue'
 import { useGeolocation } from './composables/useGeolocation'
+import { Loader } from '@googlemaps/js-api-loader'
 
 export default defineComponent({
   name: 'App',
@@ -23,8 +42,59 @@ export default defineComponent({
       lat: coords.value.latitude,
       lng: coords.value.longitude
     }))
+    // Get clicked position of the user
+    const clickedPos = ref<{
+      lat: number
+      lng: number
+    } | null>(null)
 
-    return { currentPos }
+    // Google maps loader
+    const loader = new Loader({
+      apiKey: 'AIzaSyBcO-Xg5pqUrFitJXyt0P5UEQjgja8c2WM'
+    })
+
+    // Map ref
+    const mapRef = ref(null)
+    // Map element
+    let map = ref<google.maps.Map | null>(null)
+    // Map clickListener
+    let clickListener: google.maps.MapsEventListener | null = null
+
+    onMounted(async () => {
+      // Load the mad
+      await loader.load()
+      // Get the loaded map
+      map.value = new google.maps.Map(mapRef.value!, {
+        center: currentPos.value,
+        zoom: 7
+      })
+      // Add click event to map
+      clickListener = map.value.addListener(
+        'click',
+        ({ latLng: { lat, lng } }) =>
+          (clickedPos.value = { lat: lat(), lng: lng() })
+      )
+    })
+
+    onUnmounted(async () => {
+      // Remove clickListener
+      if (clickListener) clickListener.remove()
+    })
+
+    // Watch any map or position changes
+    let line: google.maps.Polyline | null = null
+    watch([map, currentPos, clickedPos], () => {
+      // If a line is already drawn, remove it
+      if (line) line.setMap(null)
+      // Draw line from users position to clicked position
+      if (map.value && clickedPos.value !== null)
+        line = new google.maps.Polyline({
+          path: [currentPos.value, clickedPos.value],
+          map: map.value
+        })
+    })
+
+    return { currentPos, mapRef, clickedPos }
   }
 })
 </script>
